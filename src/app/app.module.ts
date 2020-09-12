ab03c06dc633440a0a41e6f062d99a0dd666a114
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2020-09-11 22:24:50 -0500

    Se añade componente de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ProductsComponent } from './../products/products.component';
+import { ProductsService } from './../products/shared/services/products.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,14 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ProductsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
